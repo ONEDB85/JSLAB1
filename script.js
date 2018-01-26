4df4afa0e45d9a65d8947993cac03a9897887540
D'Lorean Bell <dloreanbell@gmail.com>
2018-01-26 03:39:47 -0500

    JSLAB1

---
+++ script.js
@@ -0,0 +1,28 @@
+//exercise 1
+
+
+var game = prompt("Do you want to play a game?");
+ 
+
+ if (game.toLowerCase() === "yes"){
+
+
+ 	var user = prompt("What is your name?")
+
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
